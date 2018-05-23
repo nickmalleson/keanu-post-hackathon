4c9034cc40163e793e2839ba983e5e51158a8bbe
dmculley <daveculley@improbable.io>
2018-05-23 13:24:29 +0100

    Working on MixedBlackBoxTest

--- keanu-project/src/test/java/io/improbable/keanu/research/MixedBlackBoxTest.java
+++ keanu-project/src/test/java/io/improbable/keanu/research/MixedBlackBoxTest.java
@@ -1,31 +1,81 @@
 package io.improbable.keanu.research;
 
+import io.improbable.keanu.algorithms.NetworkSamples;
+import io.improbable.keanu.algorithms.mcmc.MetropolisHastings;
+import io.improbable.keanu.network.BayesNet;
 import io.improbable.keanu.randomfactory.RandomFactory;
+import io.improbable.keanu.vertices.Vertex;
+import io.improbable.keanu.vertices.dbl.DoubleVertex;
+import io.improbable.keanu.vertices.dbl.probabilistic.GaussianVertex;
+import io.improbable.keanu.vertices.intgr.IntegerVertex;
+import io.improbable.keanu.vertices.intgr.nonprobabilistic.ConstantIntegerVertex;
+import io.improbable.keanu.vertices.intgr.probabilistic.PoissonVertex;
+import org.apache.commons.math3.util.Pair;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class MixedBlackBoxTest {
 
-    public static MixedModelIO model(ArrayList<Integer> integerInputs,
-                                     ArrayList<Double> doubleInputs,
-                                     RandomFactory<Double> random) {
+    public static Pair<Integer[], Double[]> model(Integer[] integerInputs,
+                                                  Double[] doubleInputs,
+                                                  RandomFactory<Double> random) {
 
-        ArrayList<Integer> intsOut = new ArrayList<>();
-        ArrayList<Double> dubsOut = new ArrayList<>();
+        Integer[] intsOut = new Integer[2];
+        Double[] dubsOut = new Double[2];
 
         Integer intTotal = 0;
-        for (int i=0; i<integerInputs.size(); i++) {
-            intTotal += integerInputs.get(i);
+        Integer intProduct = 1;
+        for (int input : integerInputs) {
+            intTotal += input;
+            intProduct *= input;
         }
-        intsOut.add(intTotal);
+        intsOut[0] = intTotal;
+        intsOut[1] = intProduct;
 
         Double dubTotal = 0.0;
-        for (int i=0; i<doubleInputs.size(); i++) {
-            dubTotal += doubleInputs.get(i);
+        Double dubProduct = 1.0;
+        for (double input : doubleInputs) {
+            dubTotal += input;
+            dubProduct *= input;
         }
-        dubsOut.add(dubTotal);
+        dubsOut[0] = dubTotal;
+        dubsOut[1] = dubProduct;
 
-        MixedModelIO output = new MixedModelIO(intsOut, dubsOut);
-        return output;
+        return new Pair<>(intsOut, dubsOut);
     }
+
+    public static void main (String[] args) {
+        ArrayList<DoubleVertex> doubleInputs = new ArrayList<>(2);
+        doubleInputs.add(new GaussianVertex(5.5, 3.0));
+        doubleInputs.add(new GaussianVertex(6.1, 2.0));
+
+        ArrayList<IntegerVertex> integerInputs = new ArrayList<>(2);
+        integerInputs.add(new PoissonVertex(2));
+        integerInputs.add(new PoissonVertex(3));
+
+        MixedInputOutputBlackBox box = new MixedInputOutputBlackBox(integerInputs, doubleInputs,
+            MixedBlackBoxTest::model, 2, 2);
+
+        box.fuzzyObserveOutput(0, 14.0, 0.5);
+        box.integerOutputs.get(0).observe(14);
+
+        BayesNet testNet = new BayesNet(box.getConnectedGraph());
+        ArrayList<Vertex> fromVertices = new ArrayList<>();
+        fromVertices.addAll(doubleInputs);
+        fromVertices.addAll(integerInputs);
+
+        NetworkSamples testMet = MetropolisHastings.getPosteriorSamples(testNet, fromVertices, 100);
+
+        List<Integer> inOne = testMet.get(integerInputs.get(0)).asList();
+        List<Integer> inTwo = testMet.get(integerInputs.get(1)).asList();
+
+        for (int i=0; i<inOne.size(); i++) {
+            System.out.println(inOne.get(i) + " " + inTwo.get(i) + (inOne.get(i) + inTwo.get(i)));
+        }
+
+
+    }
+
+
 }
