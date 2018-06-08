5d10e4e809f9d7961de3ae8bff9e6fe0c6dbc44a
dmculley <daveculley@improbable.io>
2018-06-08 11:17:55 +0100

    DoubleArrayIntegerVertex now compiles...

--- keanu-project/src/main/java/io/improbable/keanu/research/DoubleArrayIndexingVertex.java
+++ keanu-project/src/main/java/io/improbable/keanu/research/DoubleArrayIndexingVertex.java
@@ -1,15 +1,16 @@
 package io.improbable.keanu.research;
 
 import io.improbable.keanu.tensor.dbl.DoubleTensor;
+import io.improbable.keanu.tensor.intgr.IntegerTensor;
 import io.improbable.keanu.vertices.Vertex;
 import io.improbable.keanu.vertices.dbl.nonprobabilistic.operators.binary.DoubleBinaryOpLambda;
 import io.improbable.keanu.vertices.intgr.IntegerVertex;
 import io.improbable.keanu.vertices.intgr.nonprobabilistic.ConstantIntegerVertex;
 
-public class DoubleArrayIndexingVertex extends DoubleBinaryOpLambda<DoubleTensor[], Integer> {
+public class DoubleArrayIndexingVertex extends DoubleBinaryOpLambda<DoubleTensor[], IntegerTensor> {
 
     public DoubleArrayIndexingVertex(Vertex<DoubleTensor[]> input, IntegerVertex index) {
-        super(input, index, (DoubleTensor[] in, IntegerVertex i) -> in[i];
+        super(input, index, (DoubleTensor[] in, IntegerTensor i) -> in[i.scalar()]);
     }
 
     public DoubleArrayIndexingVertex(Vertex<DoubleTensor[]> input, Integer index) {
